3b9bb4a12d8e87c9dce732e762a1cdf1d23bdcaa
Carlos Castro <carlos.castro@mongodb.com>
2024-11-30 06:35:20 +0000

    read a file line by line;

--- d1p1/src/main.rs
+++ d1p1/src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+// The output is wrapped in a Result to allow matching on errors.
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    // check if the user has provided the input file
+    if args.len() < 2 {
+        eprintln!("Please provide the input file");
+        std::process::exit(1);
+    }
+
+    let file_path = args[1].clone();
+    if let Ok(lines) = read_lines(&file_path) {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines.flatten() {
+            println!("{}", line);
+        }
+    } else {
+        eprintln!("Error reading the file {}", file_path);
+        std::process::exit(1);
+    }
+}
+
