080e94fe32be04566bf9fd05fa5bf5192068828d
Carlos Castro <carloscastro7@gmail.com>
2024-12-01 11:20:37 +0000

    d1p1 completed;

--- d1p1/src/main.rs
+++ d1p1/src/main.rs
@@ -20,6 +20,14 @@ fn write_output_file(result: &[u8]) -> std::io::Result<()> {
     Ok(())
 }
 
+fn solve(group_1: Vec<i32>, group_2: Vec<i32>) -> i32 {
+    let mut result = 0;
+    for i in 0..group_1.len() {
+        result += group_1[i].abs_diff(group_2[i]);
+    }
+    result as i32
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     // check if the user has provided the input file
@@ -28,15 +36,26 @@ fn main() {
         std::process::exit(1);
     }
 
+    let mut group_1: Vec<i32> = Vec::new();
+    let mut group_2: Vec<i32> = Vec::new();
+
     let file_path = args[1].clone();
     if let Ok(lines) = read_lines(&file_path) {
         // Consumes the iterator, returns an (Optional) String
         for line in lines.map_while(Result::ok) {
             println!("{}", line);
+            let numbers: Vec<&str> = line.split_whitespace().collect();
+            println!("{:?}", numbers);
+
+            group_1.push(numbers[0].parse::<i32>().unwrap());
+            group_2.push(numbers[1].parse::<i32>().unwrap());
         }
-        let result: i32 = 42;
+        group_1.sort();
+        group_2.sort();
+
+        let result = solve(group_1, group_2);
         println!("Result is {:?}", result);
-        if let Err(e) = write_output_file(&result.to_ne_bytes()) {
+        if let Err(e) = write_output_file(result.to_string().as_bytes()) {
             eprintln!("Error writing the output file: {}", e);
             std::process::exit(1);
         }
