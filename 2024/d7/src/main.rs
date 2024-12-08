b35e3702309c17f25f7d60b4a6dd2cf1ec85350b
Carlos Castro <carloscastro7@gmail.com>
2024-12-08 07:09:24 +0000

    d7p2
    fastest p2 so far;

--- 2024/d7/src/main.rs
+++ 2024/d7/src/main.rs
@@ -1,9 +1,16 @@
 use std::fs::read_to_string;
 
+fn concatenate_u64(a: u64, b: u64) -> u64 {
+    let digits_in_b = (b as f64).log10().floor() as u32 + 1;
+    let multiplier = 10u64.pow(digits_in_b);
+    a * multiplier + b
+}
+
 fn apply_operation(op: &str, a: u64, b: u64) -> u64 {
     match op {
         "+" => a + b,
         "*" => a * b,
+        "||" => concatenate_u64(a, b),
         _ => panic!("Invalid operation"),
     }
 }
@@ -29,7 +36,7 @@ fn calculate(values: &[u64], operations: &[&str]) -> u64 {
     total
 }
 
-fn is_solvable(total: u64, values: &[u64]) -> bool {
+fn is_solvable_p1(total: u64, values: &[u64]) -> bool {
     // using all the values in the list and only the add and multiply operations
     // can we reach the total?
     let num_operations = values.len() - 1;
@@ -59,10 +66,47 @@ fn is_solvable(total: u64, values: &[u64]) -> bool {
     false
 }
 
-fn solve_p1(calibration: Vec<(u64, Vec<u64>)>) -> u64 {
+fn is_solvable_p2(total: u64, values: &[u64]) -> bool {
+    // using all the values in the list and only the add and multiply operations
+    // can we reach the total?
+    let num_operations = values.len() - 1;
+    let mut operations = vec![""; num_operations];
+    (0..num_operations).for_each(|i| {
+        operations[i] = "+";
+    });
+
+    // generate all possible combinations of operations
+    // + * ||
+    for i in 0..3usize.pow(num_operations as u32) {
+        let mut op = i;
+        (0..num_operations).for_each(|j| {
+            match op % 3 {
+                0 => operations[j] = "+",
+                1 => operations[j] = "*",
+                2 => operations[j] = "||",
+                _ => panic!("Invalid operation"),
+            }
+            op /= 3;
+        });
+        if calculate(values, &operations) == total {
+            return true;
+        }
+    }
+
+    false
+}
+
+fn solve_p1(calibration: &[(u64, Vec<u64>)]) -> u64 {
     calibration
         .iter()
-        .filter(|(total, values)| is_solvable(*total, values))
+        .filter(|(total, values)| is_solvable_p1(*total, values))
+        .fold(0, |acc, calibration| acc + calibration.0)
+}
+
+fn solve_p2(calibration: &[(u64, Vec<u64>)]) -> u64 {
+    calibration
+        .iter()
+        .filter(|(total, values)| is_solvable_p2(*total, values))
         .fold(0, |acc, calibration| acc + calibration.0)
 }
 
@@ -88,5 +132,6 @@ fn main() {
         .collect::<Vec<_>>();
 
     // println!("{:?}", calibration);
-    println!("{:?}", solve_p1(calibration));
+    println!("P1: {:?}", solve_p1(&calibration));
+    println!("P2: {:?}", solve_p2(&calibration));
 }
