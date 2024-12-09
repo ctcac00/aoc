52cd4e577f8c90cce0403eed1ae4cbf8e0b96b08
Carlos Castro <carlos.castro@mongodb.com>
2024-12-09 12:10:39 +0000

    d8p1;

--- 2024/d8/src/main.rs
+++ 2024/d8/src/main.rs
@@ -3,48 +3,66 @@ use std::{
     fs::read_to_string,
 };
 
-fn find_antennas(map: &[Vec<char>]) -> HashMap<char, Vec<(usize, usize)>> {
+#[derive(Debug, Eq, PartialEq, Hash, Copy, Clone)]
+struct Pos {
+    x: i32,
+    y: i32,
+}
+
+fn find_antennas(map: &[Vec<char>]) -> HashMap<char, Vec<Pos>> {
     let mut antennas = HashMap::new();
     for (i, row) in map.iter().enumerate() {
         for (j, cell) in row.iter().enumerate() {
             if *cell != '.' {
-                antennas.entry(*cell).or_insert_with(Vec::new).push((i, j));
+                antennas.entry(*cell).or_insert_with(Vec::new).push(Pos {
+                    x: i as i32,
+                    y: j as i32,
+                });
             }
         }
     }
     antennas
 }
 
-fn find_antinodes(
-    map_size: (usize, usize),
-    antenna_a: (usize, usize),
-    antenna_b: (usize, usize),
-) -> Vec<(i32, i32)> {
+fn find_antinodes(map_size: Pos, antenna_a: Pos, antenna_b: Pos) -> Vec<Pos> {
     let mut antinodes = Vec::new();
-    let mut antinode_1: (i32, i32) = (0, 0);
-    let mut antinode_2: (i32, i32) = (0, 0);
+    let mut antinode_1 = Pos { x: 0, y: 0 };
+    let mut antinode_2 = Pos { x: 0, y: 0 };
 
     println!("Antennas {:?} {:?}", antenna_a, antenna_b);
 
-    //convert to i32 to avoid negative values
-    let antenna_a = (antenna_a.0 as i32, antenna_a.1 as i32);
-    let antenna_b = (antenna_b.0 as i32, antenna_b.1 as i32);
+    let dx = antenna_b.x - antenna_a.x;
+    let dy = antenna_b.y - antenna_a.y;
 
-    // TODO: calculate the antinodes points
+    if dx >= 0 {
+        antinode_1.x = antenna_a.x - dx.abs();
+        antinode_2.x = antenna_b.x + dx.abs();
+    } else {
+        antinode_1.x = antenna_a.x + dx.abs();
+        antinode_2.x = antenna_b.x - dx.abs();
+    }
+
+    if dy >= 0 {
+        antinode_1.y = antenna_a.y - dy.abs();
+        antinode_2.y = antenna_b.y + dy.abs();
+    } else {
+        antinode_1.y = antenna_a.y + dy.abs();
+        antinode_2.y = antenna_b.y - dy.abs();
+    }
 
     // if antinode_1 is in bounds
-    if antinode_1.0 >= 0
-        && antinode_1.0 < map_size.0 as i32
-        && antinode_1.1 >= 0
-        && antinode_1.1 < map_size.1 as i32
+    if antinode_1.x >= 0
+        && antinode_1.x < map_size.x
+        && antinode_1.y >= 0
+        && antinode_1.y < map_size.y
     {
         antinodes.push(antinode_1);
     }
     // if antinode_2 is in bounds
-    if antinode_2.0 >= 0
-        && antinode_2.0 < map_size.0 as i32
-        && antinode_2.1 >= 0
-        && antinode_2.1 < map_size.1 as i32
+    if antinode_2.x >= 0
+        && antinode_2.x < map_size.x
+        && antinode_2.y >= 0
+        && antinode_2.y < map_size.y
     {
         antinodes.push(antinode_2)
     }
@@ -66,12 +84,18 @@ fn solve_p1(map: &mut [Vec<char>]) -> i32 {
         // for each pair of antennas
         for i in 0..antenna.len() {
             for j in i + 1..antenna.len() {
-                let new_antinodes =
-                    find_antinodes((map.len(), map[0].len()), antenna[i], antenna[j]);
+                let new_antinodes = find_antinodes(
+                    Pos {
+                        x: map.len() as i32,
+                        y: map[0].len() as i32,
+                    },
+                    antenna[i],
+                    antenna[j],
+                );
 
                 // add anti nodes to map
                 for antinode in new_antinodes.iter() {
-                    map[antinode.0 as usize][antinode.1 as usize] = '#';
+                    map[antinode.x as usize][antinode.y as usize] = '#';
                 }
                 print_map(map);
 
@@ -92,7 +116,10 @@ fn solve_p1(map: &mut [Vec<char>]) -> i32 {
 
 fn print_map(map: &mut [Vec<char>]) {
     for row in map {
-        println!("{:?}", row);
+        for cell in row {
+            print!("{}", cell);
+        }
+        println!();
     }
     println!();
 }
